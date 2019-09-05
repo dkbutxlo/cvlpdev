c37e76bf6e1df6a4c9572bf8a500bd71588b84b2
dkbutxlo <butelo.dawid@googlemail.com>
2019-09-05 23:04:55 +0200

    Noch mehr Animationen

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 require('normalize.css/normalize.css');
 require('./styles/index.scss');
 require('./assets/8306-programming-animation.json'); 
+require('./assets/d.json'); 
+require('./assets/github.json'); 
 import lottie from 'lottie-web';
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -11,6 +13,20 @@ document.addEventListener("DOMContentLoaded", () => {
         loop: true,
         autoplay: true,
         path: '8306-programming-animation.json' // the path to the animation json
+      });
+      lottie.loadAnimation({
+        container: document.getElementById('d'), // the dom element that will contain the animation
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'd.json' // the path to the animation json
+      });
+      lottie.loadAnimation({
+        container: document.getElementById('git'), // the dom element that will contain the animation
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'github.json' // the path to the animation json
       });
 });
 
