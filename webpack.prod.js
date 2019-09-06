4428f7a639a34233084dc95552ef9edc3a52d714
dkbutxlo <butelo.dawid@googlemail.com>
2019-09-06 13:39:41 +0200

    Workbox erneut entfernt

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,6 @@ const CleanWebpackPlugin = require('clean-webpack-plugin'); //installed via npm
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const buildPath = path.resolve(__dirname, 'dist');
 
@@ -155,7 +154,6 @@ module.exports = {
                 discardUnused: false
             },
             canPrint: true
-        }),
-        new WorkboxPlugin()
+        })
     ]
 };
