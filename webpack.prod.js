553e1c72c0bfe07c5ab6a42e215c9358384ea756
dkbutxlo <butelo.dawid@googlemail.com>
2019-09-06 13:24:59 +0200

    Workbox is back

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin'); //installed via npm
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const buildPath = path.resolve(__dirname, 'dist');
 
@@ -154,6 +155,7 @@ module.exports = {
                 discardUnused: false
             },
             canPrint: true
-        })
+        }),
+        new WorkboxPlugin()
     ]
 };
