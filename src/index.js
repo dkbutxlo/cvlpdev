082d8078164301ad211fb5ec0d85d81f8d48cb4a
dkbutxlo <butelo.dawid@googlemail.com>
2019-09-09 21:09:45 +0200

    removed some things

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 
 require('normalize.css/normalize.css');
 require('./styles/index.scss');
-require('./assets/8306-programming-animation.json');
 require('./assets/d.json');
 require('./assets/github.json');
 require('./assets/bear.gif');
 require('./assets/waves.json');
 import lottie from 'lottie-web';
-import modularLoad from 'modularload';
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    lottie.loadAnimation({
-        container: document.getElementById('bm'), // the dom element that will contain the animation
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: '8306-programming-animation.json' // the path to the animation json
-      });
       lottie.loadAnimation({
         container: document.getElementById('d'), // the dom element that will contain the animation
         renderer: 'svg',
