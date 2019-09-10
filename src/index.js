d2c031afe633084f90dbc1871bfd47db438a74f2
dkbutxlo <butelo.dawid@googlemail.com>
2019-09-10 11:22:48 +0200

    Deactivate all

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
 require('normalize.css/normalize.css');
 require('./styles/index.scss');
-require('./assets/d.json');
-require('./assets/waves.json');
+// require('./assets/d.json');
+// require('./assets/waves.json');
 
 import lottie from 'lottie-web';
 import AOS from 'aos';
@@ -17,22 +17,22 @@ AOS.init();
 
 document.addEventListener("DOMContentLoaded", () => {
 
-      lottie.loadAnimation({
-        container: document.getElementById('d'), // the dom element that will contain the animation
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: 'd.json' // the path to the animation json
-      });
-
-
-      lottie.loadAnimation({
-        container: document.getElementById('waves'), // the dom element that will contain the animation
-        renderer: 'svg',
-        loop: false,
-        autoplay: true,
-        path: 'waves.json' // the path to the animation json
-      });
+      // lottie.loadAnimation({
+      //   container: document.getElementById('d'), // the dom element that will contain the animation
+      //   renderer: 'svg',
+      //   loop: true,
+      //   autoplay: true,
+      //   path: 'd.json' // the path to the animation json
+      // });
+      //
+      //
+      // lottie.loadAnimation({
+      //   container: document.getElementById('waves'), // the dom element that will contain the animation
+      //   renderer: 'svg',
+      //   loop: false,
+      //   autoplay: true,
+      //   path: 'waves.json' // the path to the animation json
+      // });
 
 
 
