e0326ec5a7e9c7effe5ec71abbaababbbe5dc1e5
dkbutxlo <butelo.dawid@googlemail.com>
2019-10-08 13:50:52 +0200

    CURSOR EFFEKT
    <3

--- src/index.js
+++ src/index.js
@@ -16,6 +16,21 @@ observer.observe();
 AOS.init();
 luxy.init();
 
+document.onmousemove = function(e) {
+      document.body.style.setProperty("--x", e.clientX + "px");
+      document.body.style.setProperty("--y", e.clientY + "px");
+    };
+    
+    // hover effect
+//     const clickableElements = document.querySelectorAll(".clickable");
+//     clickableElements.forEach(elm => {
+//       elm.addEventListener("mouseover", function() {
+//         document.body.classList.add("img-hovered");
+//       });
+//       elm.addEventListener("mouseleave", function() {
+//         document.body.classList.remove("img-hovered");
+//       });
+//     });
 
 document.addEventListener("DOMContentLoaded", () => {
 
