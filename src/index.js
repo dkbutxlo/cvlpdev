1cfb4bd650142b1a4e62041b3506fc9afcf014e3
dkbutxlo <butelo.dawid@googlemail.com>
2020-03-10 09:26:26 +0100

    Opti

--- src/index.js
+++ src/index.js
@@ -3,37 +3,27 @@ require('normalize.css/normalize.css');
 require('./styles/index.scss');
 require('./assets/d.json');
 
-// require('./assets/waves.json');
 import Analytics from 'analytics'
 import googleAnalytics from '@analytics/google-analytics'
 import lottie from 'lottie-web';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-
-
-
-
-
-document.onmousemove = function(e) {
-      document.body.style.setProperty("--x", e.clientX + "px");
-      document.body.style.setProperty("--y", e.clientY + "px");
-    };
-
-    const analytics = Analytics({
-      app: 'awesome-app',
-      plugins: [
-        googleAnalytics({
-          trackingId: 'UA-159238801-1'
-        })
-      ]
+const analytics = Analytics({
+  app: 'awesome-app',
+  plugins: [
+    googleAnalytics({
+      trackingId: 'UA-159238801-1'
     })
-    
-    /* Track a page view */
-    analytics.page()
-    /* Start animations */
-    AOS.init();
-    
+  ]
+})
+
+/* Track a page view */
+analytics.page()
+/* Start animations */
+AOS.init();
+
+
 document.addEventListener("DOMContentLoaded", () => {
 
       lottie.loadAnimation({
@@ -43,6 +33,11 @@ document.addEventListener("DOMContentLoaded", () => {
         autoplay: true,
         path: 'd.json' // the path to the animation json
       });
+
+      document.onmousemove = function(e) {
+        document.body.style.setProperty("--x", e.clientX + "px");
+        document.body.style.setProperty("--y", e.clientY + "px");
+      };
 
      
       var clickableElements = document.querySelectorAll(".clickable");
